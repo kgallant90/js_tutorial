c8544fb91e5789a9d1eaa015f29d592c035ff4eb
Kelton Gallant <kgallant90@gmail.com>
2020-07-03 14:18:05 -0400

    Palidrome returns null for whitespace

--- main.js
+++ main.js
@@ -1,19 +1,46 @@
 let Phrase = require("kelton_gallant-palindrome");
 
-function palindromeTester () {
-  let string = prompt("Please enter a string for palindrome testing:");
-  let phrase = new Phrase(string);
+function palindromeTester (event) {
+  event.preventDefault();
+
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome.`);
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  let button = document.querySelector("#palindromeTester");
-  button.addEventListener("click", function() {
-    palindromeTester();
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function(event) {
+    palindromeTester(event);
   });
 });
+
+
+//
+// Prompt user to enter information (as opposed to text prompt)
+//
+// let Phrase = require("<username>-palindrome");
+//
+// function palindromeTester() {
+//   let string = prompt("Please enter a string for palindrome testing:");
+//   let phrase = new Phrase(string);
+//   let palindromeResult = document.querySelector("#palindromeResult");
+//
+//   if (phrase.palindrome()) {
+//     palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+//   } else {
+//     palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+//   }
+// }
+//
+// document.addEventListener("DOMContentLoaded", function() {
+//   let button = document.querySelector("#palindromeTester");
+//   button.addEventListener("click", function() {
+//     palindromeTester();
+//   });
+// });
